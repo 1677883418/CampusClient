e7660dcda6ff89a2a6b7331d8788a363b5bbdeae
goodsir <zxy1677883418@gmail.com>
2021-10-15 00:01:16 +0800

    初始化小程序端仓库

--- pages/home/mine/home/home.js
+++ pages/home/mine/home/home.js
@@ -1,11 +1,8 @@
 const app = getApp();
-Component({
-    options: {
-        addGlobalClass: true,
-    },
+Page({
     data: {
         user: {
-            nikeName: wx.getStorageSync("nikeName"),
+            nikeName: '',
             avatarUrl: wx.getStorageSync("avatarUrl"),
             isCard: true,
             cardId: '',
@@ -15,8 +12,30 @@ Component({
             reply: '100'
         }
     },
-    onload() {
+    onLoad() {
 
+    },
+    getInfo: function (e) {
+        // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认
+        // 开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
+        wx.getUserProfile({
+            desc: '用于完善会员资料', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+            withCredentials: true,
+            lang: 'zh_CN',
+            success: (res) => {
+                //获取到
+                this.setData({
+                    user: res.userInfo,
+                })
+                wx.setStorageSync("userInfo", res.userInfo)
+                app.com.post("/User/updateUser", {user: this.data.user}, function (res) {
+                    if (res.data === 1) {
+
+                    }
+                })
+            }
+        })
     }
+
 
 })
